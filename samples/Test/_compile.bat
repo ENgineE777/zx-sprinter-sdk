46a69c20b0beb23f7a9bab7266b8cfd4793f5d2d
ESoul <enginee777@gmail.com>
2022-09-29 22:14:57 -0700

    Compiling gfx.dat in _compile.bat

--- samples/Test/_compile.bat
+++ samples/Test/_compile.bat
@@ -1,13 +1,29 @@
 
-path=..\..\sdk\sdcc\bin;..\..\sdk\bin;
-
 set temp=_temp_
 
+echo Building gfx.dat...
+
+CD Sprites
+
+..\..\..\sdk\bin\mkimg.exe img.lst gfx.dat
+
+CD ..\
+
+echo Building wyz.bin (TODO)...
+
+echo Compiling code...
+
 mkdir %temp%
-sdcc -mz80 --code-loc 0x0006 --data-loc 0 --max-allocs-per-node 5000 --no-std-crt0 -I..\..\sdk\include ..\..\sdk\bin\crt0.rel ..\..\sdk\bin\sprinter.rel --opt-code-speed main.c -o %temp%\out.ihx
+..\..\sdk\sdcc\bin\sdcc -mz80 --code-loc 0x0006 --data-loc 0 --max-allocs-per-node 5000 --no-std-crt0 -I..\..\sdk\include ..\..\sdk\bin\crt0.rel ..\..\sdk\bin\sprinter.rel --opt-code-speed main.c -o %temp%\out.ihx
 
-mkcode %temp%\out.ihx ..\..\sdk\bin\startup.bin
+echo Converting out.ihx...
 
-copy /b /y ..\..\sdk\bin\loader.exe + code.bin + Sound\wyz.bin + Sprites\gfx.dat hello.exe
+CD %temp%
+
+..\..\..\sdk\bin\mkcode.exe out.ihx ..\..\..\sdk\bin\startup.bin
+
+echo Creating exe...
+
+copy /b /y ..\..\..\sdk\bin\loader.exe + code.bin + ..\Sound\wyz.bin + ..\Sprites\gfx.dat ..\Test.exe
 
 pause 0

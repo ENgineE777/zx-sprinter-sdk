d19eaf112c48c6ed3f220a5fc6722fde7f9d215b
ESoul <enginee777@gmail.com>
2022-10-01 18:37:11 -0700

    Making spaceship dynamic

--- samples/Spaceship/main.c
+++ samples/Spaceship/main.c
@@ -13,15 +13,13 @@
 #define BIG_ROCK_TIMER rand8() % 64 + 32
 #define STAR_SPEED rand8() % 8 + 2
 
+#define SPACESHIP_X 60
+#define SPACESHIP_Y 75.0f
+#define SPACESHIP_WAVE_HEIGHT 35.0f
+
 u8 rand8()
 {
     return (u8)(rand16());
-}
-
-void draw_ship(u8 x,u8 y)
-{
-    add_sprite(x, y, 3);
-    add_sprite(x + 16, y, 4);
 }
 
 void make_black()
@@ -39,16 +37,16 @@ typedef struct
     u8 x, y, speed, sprite;
 } Star;
 
-u8 stars_count = 8;
-Star stars[8];
+u8 stars_count = 16;
+Star stars[16];
 
 typedef struct
 {
     u8 x, y, speed;
 } Rock;
 
-u8 rocks_count = 3;
-Rock rocks[3];
+u8 rocks_count = 4;
+Rock rocks[4];
 
 typedef struct
 {
@@ -58,6 +56,8 @@ typedef struct
 
 int big_rock_timer;
 BigRock big_rock;
+
+u8 spaceship_timer = 0;
 
 void draw_big_rock()
 {
@@ -159,6 +159,7 @@ void main()
     init();
 
     u8 i;
+    u8 spaceship_y;
 
     while (1)
     {
@@ -215,7 +216,7 @@ void main()
 
                 big_rock.x = 255;
                 big_rock.y = rand8() % 144;
-                big_rock.speed = 1;
+                big_rock.speed = BIG_ROCK_SPEED;
             }
             else
             {
@@ -226,7 +227,31 @@ void main()
         }
         
         //ship
-        draw_ship(30, 90);
+        spaceship_timer++;
+
+        float lerp = 0.0f;
+
+        if (spaceship_timer < 100)
+        {
+            float k = (float)(spaceship_timer < 50 ? spaceship_timer : (100 - spaceship_timer)) / 50.0f;
+            lerp = (1 - (1 - k) * (1 - k));
+        }
+        else
+        if (spaceship_timer < 200)
+        {
+            float k = (float)(spaceship_timer < 150 ? (spaceship_timer - 100): (200 - spaceship_timer)) / 50.0f;
+            lerp = -(1 - (1 - k) * (1 - k));
+        }        
+        else
+        {
+            spaceship_timer = 0;
+        }
+
+        lerp *= SPACESHIP_WAVE_HEIGHT;
+        lerp += SPACESHIP_Y;
+
+        add_sprite(SPACESHIP_X, (u8)lerp, 3);
+        add_sprite(SPACESHIP_X + 16, (u8)lerp, 4);
 
         end_sprite();
 

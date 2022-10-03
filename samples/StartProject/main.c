48b122b70471884350e06ee91569b7876068aaaa
ESoul <enginee777@gmail.com>
2022-10-02 18:50:50 -0700

    Start project in samples

---
+++ samples/StartProject/main.c
@@ -0,0 +1,16 @@
+#include "sprinter.h"
+#include "splib.h"
+
+void main()
+{
+    sprites_start();
+
+    wyz_play_music (0);
+
+    pal_bright(15);
+    unpack_screen(0, 2);
+
+    while (1)
+    {
+    }
+}
